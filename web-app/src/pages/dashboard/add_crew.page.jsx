e7784f942e8ce76b9cd6b38c655a51c47d27cd02
Ashish Shetty <shetty073@gmail.com>
2025-05-04 19:38:35 +0530

    fix button sytling

--- web-app/src/pages/dashboard/add_crew.page.jsx
+++ web-app/src/pages/dashboard/add_crew.page.jsx
@@ -6,8 +6,8 @@ export default function AddCrewPage(params) {
       <div className="mb-3">
 
         <div className="row justify-content-between">
-          <h3 className="fw-bold fs-4 mb-3 text-capitalize col-4">Add a new crew</h3>
-          <Link to="/dashboard/crews" className="btn btn-danger col-2 mb-3">Cancel</Link>
+          <h3 className="fw-bold fs-4 mb-3 text-capitalize col-6 col-md-4">Add a new crew</h3>
+          <Link to="/dashboard/crews" className="btn btn-danger col-6 col-lg-2 mb-3">Cancel</Link>
         </div>
 
         <div className="row">
