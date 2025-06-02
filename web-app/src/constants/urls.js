9cf223be0306851e9df3c845a63a8bf82f06b3c7
Ashish Shetty <shetty073@gmail.com>
2025-06-02 15:31:24 +0530

    add proper way of handling logouts

--- web-app/src/constants/urls.js
+++ web-app/src/constants/urls.js
@@ -1,2 +1,8 @@
 
 export const BACKEND_BASE_URI = 'http://localhost:8000';
+
+export const ENDPOINTS = {
+    LOGIN: "/identity/api/login/",
+    LOGOUT: "/identity/api/logout/",
+    LOGOUT_ALL: "/identity/api/logout/all/",
+};
--- web-app/src/contexts/authContext.jsx
+++ web-app/src/contexts/authContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
+import axiosClient from '../utils/axiosClient';
 
 const AuthContext = createContext();
 
@@ -8,18 +9,37 @@ export const AuthProvider = ({ children }) => {
     return storedUser ? JSON.parse(storedUser) : null;
   });
 
-  const login = (userData) => {
+  const [token, setToken] = useState(() => {
+    const storedToken = sessionStorage.getItem("authToken");
+    return storedToken ? storedToken : null;
+  });
+
+  const login = (userData, token) => {
     setUser(userData);
     sessionStorage.setItem("user", JSON.stringify(userData));
+
+    setToken(token);
+    sessionStorage.setItem('authToken', token);
   };
 
-  const logout = () => {
-    setUser(null);
-    sessionStorage.removeItem("user");
+  const logout = async (all = false) => {
+    endpoint = ENDPOINTS.LOGOUT;
+    if (all === true) {
+      endpoint = ENDPOINTS.LOGOUT_ALL;
+    }
+    const response = await axiosClient.post(endpoint);
+
+    if (response.status === 200) {
+      setUser(null);
+      sessionStorage.removeItem("user");
+  
+      setToken(null);
+      sessionStorage.removeItem("authToken");
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, token, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
