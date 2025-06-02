d82185da43a3cd41491c260f36cbb06abfda9563
Ashish Shetty <shetty073@gmail.com>
2025-06-02 14:42:52 +0530

    add search bar for users

--- web-app/src/pages/dashboard/add_crew.page.jsx
+++ web-app/src/pages/dashboard/add_crew.page.jsx
@@ -7,11 +7,14 @@ export default function AddCrewPage(params) {
 
         <div className="row justify-content-between">
           <h3 className="fw-bold fs-4 mb-3 text-capitalize col-6 col-md-4">Add a new crew</h3>
-          <Link to="/dashboard/crews" className="btn btn-danger col-6 col-lg-2 mb-3">Cancel</Link>
+          <Link to="/dashboard/crew" className="btn btn-danger col-6 col-lg-2 mb-3">Cancel</Link>
         </div>
 
         <div className="row">
-          Forms go here...
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="basic-addon1">@</span>
+          <input type="text" className="form-control" placeholder="Search users by username, email or phone number" aria-label="User search bar" aria-describedby="basic-addon1" />
+        </div>
         </div>
 
       </div>
